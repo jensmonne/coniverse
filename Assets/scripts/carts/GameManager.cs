85293ec1d27884232ff7e3c1248d8fd9f765e318
daan <Daangulitz@gmail.com>
2025-02-04 14:31:27 +0100

    GameManager

--- Assets/scripts/carts/GameManager.cs
+++ Assets/scripts/carts/GameManager.cs
@@ -1,16 +1,61 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
-    private int currentRound = 0;
-    
-    private void Start()
+    public static GameManager Instance;
+
+    public int totalRounds;  // Total number of rounds
+    public int currentRound; // Tracks the current round
+    public int playerCount;  // Number of players
+    public List<int> playerScores; // List of player scores
+
+    // makes sure that there's always a GameManager in every scene
+    private void Awake()
     {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); 
+        }
+        else
+        {
+            Destroy(gameObject); 
+        }
+    }
+    
+
+    // sets the counts for the game
+    public void InitializeGame(int numPlayers)
+    {
+        playerCount = numPlayers; 
+        totalRounds = 3; 
+        currentRound = 1; 
+
+        // Initialize player scores
+        playerScores = new List<int>(new int[playerCount]);
         
     }
-
-    private void Update()
+    
+    // Add's the deaths to the right player
+    public void AddScore(int winnerIndex, int score)
     {
-        
+        if (winnerIndex < 0 || winnerIndex >= playerScores.Count) 
+        {
+            Debug.LogError($"Invalid winner index: {winnerIndex}");
+            return;
+        }
+
+        playerScores[winnerIndex] += score; 
+    }
+
+    public bool IsGameOver()
+    {
+        return currentRound >= totalRounds; 
+    }
+
+    public void NextRound()
+    {
+        currentRound++; 
     }
 }
---
+++ Assets/scripts/carts/GameRoundManager.cs
@@ -0,0 +1,100 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.SceneManagement;
+
+public class GameRoundManager : MonoBehaviour
+{
+    public List<PlayerHealth> players;
+    public static GameRoundManager Instance;
+
+    private void Start()
+    {
+
+        players = new List<PlayerHealth>(FindObjectsOfType<PlayerHealth>());
+
+
+        for (int i = 0; i < players.Count; i++)
+        {
+            players[i].PlayerID = i;  
+        }
+
+        Debug.Log($"Round {GameManager.Instance.currentRound} of {GameManager.Instance.totalRounds}");
+    }
+
+    public void CheckForRoundWinner()
+{
+    PlayerHealth survivingPlayer = null;
+    
+    foreach (var player in players)
+    {
+        if (player.IsAlive)
+        {
+            survivingPlayer = player;
+            break; 
+        }
+    }
+
+    
+    if (survivingPlayer != null)
+    {
+        // Award points to the player(s) who died
+        GameManager.Instance.AddScore(survivingPlayer.PlayerID, 1);
+        Debug.Log($"Surviving Player: {survivingPlayer.gameObject.name} awarded 1 point.");
+    }
+    else
+    {
+        Debug.LogWarning("No surviving player found! Something went wrong.");
+    }
+
+    if (GameManager.Instance.IsGameOver())
+    {
+        Debug.Log("Game Over");
+    }
+    else
+    {
+        // Proceed to the next round
+        GameManager.Instance.NextRound();
+        SceneManager.LoadScene("BumperCasr");
+    }
+}
+
+
+    private void PlayerWins(PlayerHealth winner)
+    {
+        int winnerIndex = winner.PlayerID; 
+
+        if (winnerIndex < 0 || winnerIndex >= GameManager.Instance.playerScores.Count)
+        {
+            Debug.LogError("Winner's index is invalid!");
+            return;
+        }
+
+        Debug.Log($"{winner.gameObject.name} wins the round!");
+
+        // Assign the score immediately
+        GameManager.Instance.AddScore(winnerIndex, 1);
+
+        Debug.Log($"Player {winnerIndex} score updated: {GameManager.Instance.playerScores[winnerIndex]}");
+
+        // Proceed to the next round after a delay so the animations can play
+        StartCoroutine(GoToNextRoundAfterDelay(3f));
+    }
+
+    private IEnumerator GoToNextRoundAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        if (GameManager.Instance.IsGameOver())
+        {
+            Debug.Log('Game Over');
+        }
+        else
+        {
+            // sets the new round up
+            GameManager.Instance.NextRound();
+            SceneManager.LoadScene("GameScene");
+        }
+    }
+}
